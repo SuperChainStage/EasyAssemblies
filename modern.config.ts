9f17251aeeb13b92a53e648b5ba0f26a82bbe477
Kevin4077 <1394506186@qq.com>
2026-04-01 05:26:52 +0800

    upd: update build process and adjust output paths for production environment

--- modern.config.ts
+++ modern.config.ts
@@ -1,15 +1,17 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   output: {
     distPath: {
-      root: 'docs',
+      root: isProd ? 'docs' : 'dist',
       html: './', 
     },
-    assetPrefix: '/EasyAssemblies/',
+    assetPrefix: isProd ? '/EasyAssemblies/' : '/',
   },
   server: {
-    baseUrl: '/EasyAssemblies/',
+    baseUrl: isProd ? '/EasyAssemblies/' : '/',
   },
   html: {
     outputStructure: 'flat',
